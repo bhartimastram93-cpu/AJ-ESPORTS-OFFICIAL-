4af5b471b502ca645de83399de7aee3dc74051b4
bhartimastram93-cpu <bhartimastram93@gmail.com>
2026-09-14 13:41:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.SUPABASE_URL = "https://otjwqfxvaxfgjibwtlkq.supabase.co";
 
-window.SUPABASE_ANON_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90andxZnh2YXhmZ2ppYnd0bGtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1MzYzMTEsImV4cCI6MjEwNDExMjMxMX0.-7c1LCvwcI0QD6-ZT5aBKAmVouuLU2HU_HTKkLFnbLc";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90andxZnh2YXhmZ2ppYnd0bGtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1MzYzMTEsImV4cCI6MjEwNDExMjMxMX0.-7c1LCvwcI0QD6-ZT5aBKAmVouuLU2HU_HTKkLFnbLc";
 
 window.UPI_ID = "ajesportsofficial@ibl";
-window.ADMIN_EMAIL = "bhartiamarjeet619@gmail.com"
+
+window.ADMIN_EMAIL = "bhartiamarjeet619@gmail.com";
